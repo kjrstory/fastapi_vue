438a0e9696485d39ffec7ff09337ed970e477ff4
kjrstory <kjrstory@gmail.com>
2023-11-07 18:09:01 +0900

    v3.02 update

--- frontend/src/views/PageHome.vue
+++ frontend/src/views/PageHome.vue
@@ -18,6 +18,21 @@
     </tr>
     </tbody>
     </table>
+    <!-- 페이징처리 시작 -->
+    <ul class="pagination justify-content-center">
+      <li class="page-item" :class="{ disabled: page <= 0 }">
+        <button class="page-link" @click="getQuestionList(page - 1)">이전</button>
+      </li>
+      <template v-for="(value, index) in Array.from({ length: totalPage })" :key="index">
+        <li class="page-item" v-if="index >= page - 5 && index <= page + 5" :class="{ active: index === page }">
+          <button class="page-link" @click="getQuestionList(index)">{{ index + 1 }}</button>
+        </li>
+      </template>
+      <li class="page-item" :class="{ disabled: page >= totalPage - 1 }">
+        <button class="page-link" @click="getQuestionList(page + 1)">다음</button>
+      </li>
+    </ul>
+    <!-- 페이징처리 끝 -->
     <div class="d-flex justify-content-start">
       <router-link to="/question-create" class="btn btn-primary">질문 등록하기</router-link>
     </div>  
@@ -25,19 +40,37 @@
 </template>
 
 <script>
+  import fastapi from '../lib/api';
 
-import fastapi from '../lib/api';
-
-export default {
-  data() {
-    return {
-      questionList: []
-    };
-  },
-  created() {
-    fastapi('get', '/api/question/list', {}, (json) => {
-      this.questionList = json;
-    });
+  export default {
+    data() {
+      return {
+        questionList: [],
+        page: 0,
+        size: 10,
+        total: 0,
+      };
+    },
+    computed: {
+      totalPage() {
+        return Math.ceil(this.total / this.size);
+      },
+    },
+    methods: {
+      getQuestionList(_page) {
+        let url = "/api/question/list"
+        let params = { 
+            page: _page,
+            size: this.size}
+        fastapi('get', url, params, (json) => {
+          this.questionList = json.question_list;
+          this.page = _page;
+          this.total = json.total;
+        });
+      }
+    },
+    created() {
+      this.getQuestionList(0);
+    }
   }
-}
 </script>
