87f3fb1fd77898767c9585cc3ac2ce4d07554f38
kjrstory <kjrstory@gmail.com>
2023-11-07 17:38:27 +0900

    v3.01 update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,15 @@
 <template>
+  <NavigationBar />
   <router-view/>  
 </template>
 
 <script>
+import NavigationBar from "./components/NavigationBar.vue"
 
 export default {
   name: 'App',
   components: {
+     NavigationBar
   }
 }
 </script>
---
+++ frontend/src/components/NavigationBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">Pybo</router-link>
+      <button 
+        class="navbar-toggler" 
+        type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarNav" 
+        aria-controls="navbarNav" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/user-create" class="nav-link">회원가입</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/user-login" class="nav-link">로그인</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
