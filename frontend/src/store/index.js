72196a915a2b41b9b957f37fb903723e5abab50c
kjrstory <kjrstory@gmail.com>
2023-11-14 05:22:13 +0900

    store eroor api update

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,23 +3,16 @@ import persistedstate from 'vuex-persistedstate';
 
 
 const store = createStore({
-  plugins: [persistedstate()],
-  state: {
-    page: 0
-  },
-  mutations: {
-    setPage(state, payload) {
-      state.page = payload
-    }
-  },
-  actions: {
-    setPage(context, payload) {
-      context.commit('setPage', payload)
-    }
-  },
-  getters: {
-    getPage: (state) => state.page
-  }
+    plugins: [persistedstate()],
+    state: {
+        page: 0
+    },
+    mutations: {
+        setPage: (state, payload) => state.page = payload 
+    },
+    actions: {
+        setPage: ({commit}, payload) => commit('setPage', payload)
+    },
 })
 
 export default store
