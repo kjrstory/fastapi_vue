c37e8c9731797f0d8e2edb60a6d38e4744c4d50f
kjrstory <kjrstory@gmail.com>
2023-11-13 22:25:01 +0900

    api js fix

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,9 +1,6 @@
-import axios from 'axios'
-
 const fastapi = (operation, url, params, success_callback, failure_callback) => {
-
-    let method = operation 
-    let content_type = 'application/json' 
+    let method = operation
+    let content_type = 'application/json'
     let body = JSON.stringify(params)
 
     let _url = process.env.VUE_APP_SERVER_URL + url
@@ -12,41 +9,36 @@ const fastapi = (operation, url, params, success_callback, failure_callback) => 
     }
 
     let options = {
-      method: method,
-      headers: {
-         "Content-Type": content_type
-      }
+        method: method,
+        headers: {
+            "Content-Type": content_type
+        }
     }
 
     if (method !== 'get') {
-       options['data'] = body
+        options['body'] = body
     }
 
-    axios(_url, options)
-      .then(response => {
-         if (response.status >= 200 && response.status < 300) {
-            if (success_callback) {
-                success_callback(response.data)
-            }
-         } else {
+    fetch(_url, options)
+        .then(response => {
             response.json()
                 .then(json => {
-                    if (failure_callback) {
-                        failure_callback(json)
-                    } else {
-                        alert(JSON.stringify(json))
+                    if(response.status >= 200 && response.status < 300) {  // 200 ~ 299
+                        if(success_callback) {
+                            success_callback(json)
+                        }
+                    }else {
+                        if (failure_callback) {
+                            failure_callback(json)
+                        }else {
+                            alert(JSON.stringify(json))
+                        }
                     }
                 })
                 .catch(error => {
                     alert(JSON.stringify(error))
                 })
-        }
-       })
-      .catch(error => {
-        if (failure_callback) {
-            failure_callback(error)
-        }
-      })
+        })
 }
 
 export default fastapi
