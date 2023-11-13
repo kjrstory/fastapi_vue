ab3836e9d611ad00e44a1cbf2344a89735861b2f
kjrstory <kjrstory@gmail.com>
2023-11-13 18:01:50 +0900

    V3.01 navbarSupportedContent (#8)
    * 답변 등록 문제 해결
    
    * navbarSupportedContent
    
    * small spelling fix

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -6,13 +6,13 @@
         class="navbar-toggler" 
         type="button" 
         data-bs-toggle="collapse" 
-        data-bs-target="#navbarNav" 
-        aria-controls="navbarNav" 
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" 
         aria-expanded="false" 
         aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item" v-if="is_login">
              <a class="nav-link" href="/user-login" @click.prevent="logoutUser">로그아웃 ({{ username }})</a>
