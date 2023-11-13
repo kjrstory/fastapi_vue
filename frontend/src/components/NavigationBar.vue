6c95a5cee1b3831cdc570756e6da8098d57c3248
kjrstory <kjrstory@gmail.com>
2023-11-13 17:52:21 +0900

    small spelling fix

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -7,12 +7,12 @@
         type="button" 
         data-bs-toggle="collapse" 
         data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedConten" 
+        aria-controls="navbarSupportedContent" 
         aria-expanded="false" 
         aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedConten">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <router-link to="/user-create" class="nav-link">회원가입</router-link>
