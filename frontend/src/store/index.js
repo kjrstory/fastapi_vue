b11874ed79b6ea3468ce2ccb25aa46af3e729acc
kjrstory <kjrstory@gmail.com>
2023-11-16 09:40:52 +0900

    comment update

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,72 +3,37 @@ import persistedstate from 'vuex-persistedstate';
 
 
 const store = createStore({
-  plugins: [persistedstate()],
-  state: {
-    page: 0,
-    access_token: "",
-    username: "",
-    is_login: false,
-    keyword: "",
-    answer_page: 0,
-    sort_by: 'create_date',
-    desc: true,
-  },
-  mutations: {
-    setPage(state, payload) {
-      state.page = payload
+    plugins: [persistedstate()],
+    state: {
+        page: 0,
+        access_token: "",
+        username: "",
+        is_login: false,
+        keyword: "",
+        answer_page: 0,
+        sort_by: 'create_date',
+        desc: true,
     },
-    setAccessToken(state, token) {
-      state.access_token = token
+    mutations: {
+        setPage: (state, payload) => state.page = payload ,
+        setAccessToken: (state, payload) => state.access_token = payload, 
+        setUsername: (state, payload) => state.username = payload,
+        setIsLogin: (state, payload) => state.is_login = payload,
+        setKeyword: (state, payload) => state.keyword = payload,
+        setAnswerPage: (state, payload) => state.answer_page = payload,
+        setSortBy: (state, payload) => state.sort_by = payload,
+        setDesc: (state, payload) => state.desc = payload,
     },
-    setUsername(state, username) {
-      state.username = username
+    actions: {
+        setPage: ({commit}, payload) => commit('setPage', payload),
+        setAccessToken: ({commit}, payload) => commit('setAccessToekn', payload),
+        setUsername: ({commit}, payload) => commit('setUsername', payload),
+        setIsLogin: ({commit}, payload) => commit('setIsLogin', payload),
+        setKeyword: ({commit}, payload) => commit('setKeyword', payload),
+        setAnswerPage: ({commit}, payload) => commit('setAnswerPage', payload),
+        setSortBy: ({commit}, payload) => commit('setSortBy', payload),
+        setDesc: ({commit}, payload) => commit('setDesc', payload),
     },
-    setIsLogin(state, is_login) {
-      state.is_login = is_login
-    },
-    setKeyword(state, keyword) {
-      state.keyword = keyword
-    },
-    setAnswerPage(state, answer_page) {
-      state.answer_page = answer_page
-    },
-    setSortBy(state, sort_by) {
-      state.sort_by = sort_by
-    },
-    setDesc(state, desc) {
-      state.desc = desc
-    },
-  },
-  actions: {
-    setPage(context, payload) {
-      context.commit('setPage', payload)
-    },
-    setAccessToken(context, payload) {
-      context.commit('setAccessToken', payload)
-    },
-    setUsername(context, payload) {
-      context.commit('setUsername', payload)
-    },
-    setIsLogin(context, payload) {
-      context.commit('setIsLogin', payload)
-    },
-    setKeyword(context, payload) {
-      context.commit('setKeyword', payload)
-    },
-    setAnswerPage(context, payload) {
-      context.commit('setAnswerPage', payload)
-    },
-    setSortBy(context, payload) {
-      context.commit('setSortBy', payload)
-    },
-    setDesc(context, payload) {
-      context.commit('setDesc', payload)
-    },
-  },
-  getters: {
-    getPage: (state) => state.page,
-  }
 })
 
 export default store
--- frontend/src/views/PageDetail.vue
+++ frontend/src/views/PageDetail.vue
@@ -10,84 +10,51 @@
                     <div class="mb-2">modified at</div>
                     <div>{{ formatDate(question.modify_date) }}</div>
                 </div>
-
                 <div class="badge bg-light text-dark p-2 text-start">
                     <div class="mb-2" v-if="question.user">{{ question.user.username }}</div>
                     <div class="mb-2" v-else></div>
                     <div>{{ formatDate(question.create_date) }}</div>
                 </div>
             </div>
-
             <button class="btn btn-sm btn-outline-secondary" @click="voteQuestion(question.id)">
                추천
                <span class="badge rounded-pill bg-success">{{ question.voter.length }}</span>
             </button>
-
-            <div class="my-3" v-if="question.user && this.$store.state.username === question.user.username">
-               <router-link :to="'/question-modify/' + question.id" class="btn btn-sm btn-outline-secondary">수정</router-link>
-               <button class="btn btn-sm btn-outline-secondary" @click="deleteQuestion(question.id)">삭제</button>
+            <div class="my-3" 
+                v-if="question.user && $store.state.username === question.user.username">
+               <router-link :to="'/question-modify/' + question.id" 
+                            class="btn btn-sm btn-outline-secondary">수정</router-link>
+               <button class="btn btn-sm btn-outline-secondary" 
+                       @click="deleteQuestion(question.id)">삭제</button>
             </div>
-
         </div>
+        <CommentQuestion :question_id="question_id"/>
     </div>
 
-    <!-- 댓글(질문) 목록 -->
-      <div class="card-header">댓글 리스트</div>
-
-        <div v-if="is_login">
-          <button class="btn btn-sm btn-outline-secondary mb-3" @click="toggleCommentInput">댓글 작성</button>
-        </div>
-        <div v-if="showCommentInput">
-           <textarea v-model="commentContent" class="form-control mb-3"
-            maxlength="500"
-            placeholder="댓글을 입력하세요."></textarea>
-        <button class="btn btn-sm btn-outline-secondary mb-3" @click="postCommentQuestion">작성</button>
-        <button class="btn btn-sm btn-outline-secondary mb-3" @click="toggleCommentInput">취소</button>
-        </div>
-    <div v-if="question.comments.length > 0" class="card my-3">
-      <div v-for="(comment, index) in question.comments" :key="comment.id" class="my-1">
-        <div v-if="index === editIndex">
-          <textarea v-model="comment.content" class="form-control mb-3"
-              :disabled="is_login ? false : true"
-              maxlength="500"
-              placeholder="Edit comment"></textarea>
-          <button class="btn btn-sm btn-outline-secondary mb-3" @click="updateComment">수정</button>
-          <button class="btn btn-sm btn-outline-secondary mb-3" @click="stopEditing">취소</button>
-        </div>
-          {{comment.content}}, <em>{{comment.user.username}}</em> - <em>{{ formatDate(comment.create_date)}}</em>
-           <span v-if="is_login && !isEditing" class="text-muted">
-             <button class="btn btn-sm btn-outline-secondary" @click="startEditing(index)">수정</button>
-             <button class="btn btn-sm btn-outline-secondary" @click="deleteComment(comment.id)">삭제</button>
-          </span>
-      </div>
-    </div>
-    <!-- 끝 -->
-    
     <div class="mt-4">
       <router-link to="/" class="btn btn-secondary">
         목록으로
       </router-link>
     </div>
-
     <!-- 답변 목록 -->
-    <div class="row">
-     <div class="col-6">
-      <h5 class="border-bottom my-3 py-2 mb-0">{{total}}개의 답변이 있습니다.</h5>
-     </div>
-     <div class="col-6 d-flex justify-content-end align-items-center">
-      <button class="btn mr-2 btn-outline-primary" 
-              :class="{ active: sort_by === 'voter_count'}"
-              @click="this.$store.dispatch('setSortBy', 'voter_count');
-                      this.$store.dispatch('setDesc', true)">추천순</button>
-      <button class="btn btn-outline-primary" 
-              :class="{ active: sort_by === 'create_date' && desc === true}"
-              @click="this.$store.dispatch('setSortBy', 'create_date');
-                      this.$store.dispatch('setDesc', true)">최신순</button>
-      <button class="btn btn-outline-primary" 
-              :class="{ active: sort_by === 'create_date' && desc === false}"
-              @click="this.$store.dispatch('setSortBy', 'create_date');
-                      this.$store.dispatch('setDesc', false)">오래된순</button>
-     </div>
+     <div class="row">
+       <div class="col-6">
+        <h5 class="border-bottom my-3 py-2 mb-0">{{total}}개의 답변이 있습니다.</h5>
+       </div>
+       <div class="col-6 d-flex justify-content-end align-items-center">
+         <button class="btn mr-2 btn-outline-primary"
+                 :class="{ active: sort_by === 'voter_count'}"
+                 @click="$store.commit('setSortBy', 'voter_count');
+                         $store.commit('setDesc', true)">추천순</button>
+         <button class="btn btn-outline-primary"
+                 :class="{ active: sort_by === 'create_date' && desc === true}"
+                 @click="$store.commit('setSortBy', 'create_date');
+                         $store.commit('setDesc', true)">최신순</button>
+         <button class="btn btn-outline-primary"
+                 :class="{ active: sort_by === 'create_date' && desc === false}"
+                 @click="$store.commit('setSortBy', 'create_date');
+                         $store.commit('setDesc', false)">오래된순</button>
+       </div>
     </div>
     <div v-for="answer in answerList" :key="answer.id" class="card my-3">
         <div class="card-body">
@@ -98,49 +65,56 @@
                     <div>{{ formatDate(answer.modify_date) }}</div>
                 </div>
                 <div class="badge bg-light text-dark p-2 text-start">
-                   <div class="mb-2" v-if="answer.user">{{ answer.user.username }}</div>
-                   <div class="mb-2" v-else></div>
-                   <div>{{ formatDate(question.create_date) }}</div>
+                    <div class="mb-2" v-if="answer.user">{{ answer.user.username }}</div>
+                    <div class="mb-2" v-else></div>
+                    <div>{{ formatDate(answer.create_date) }}</div>
                 </div>
             </div>
-
             <button class="btn btn-sm btn-outline-secondary" @click="voteAnswer(answer.id)">
                추천
                <span class="badge rounded-pill bg-success">{{ answer.voter.length }}</span>
             </button>
-
-            <div class="my-3" v-if="answer.user && this.$store.state.username === answer.user.username">
-               <router-link :to="'/answer-modify/' + answer.id" class="btn btn-sm btn-outline-secondary">수정</router-link>
-               <button class="btn btn-sm btn-outline-secondary" @click="deleteAnswer(answer.id)">삭제</button>
+            <div class="my-3" 
+                 v-if="answer.user && $store.state.username === answer.user.username">
+               <router-link :to="'/answer-modify/' + answer.id" 
+                            class="btn btn-sm btn-outline-secondary">수정</router-link>
+               <button class="btn btn-sm btn-outline-secondary"
+                       @click="deleteAnswer(answer.id)">삭제</button>
             </div>
-
         </div>
+        <CommentAnswer :answer_id="answer.id"/>
     </div>
-
     <!-- 페이징처리 시작 -->
     <ul class="pagination justify-content-center">
       <li class="page-item" :class="{ disabled: answer_page <= 0 }">
-        <button class="page-link" @click="this.$store.dispatch('setAnswerPage', answer_page - 1)">이전</button>
+        <button class="page-link" 
+                @click="$store.commit('setAnswerPage', answer_page - 1)">이전</button>
       </li>
-      <template v-for="(value, index) in Array.from({ length: totalPage })" :key="index">
-        <li class="page-item" v-if="index >= answer_page - 5 && index <= answer_page + 5" :class="{ active: index === answer_page }">
-          <button class="page-link" @click="this.$store.dispatch('setAnswerPage', index)">{{ index + 1 }}</button>
+      <template v-for="(_, loop_page) in Array.from({ length: totalPage })" :key="loop_page">
+        <li class="page-item" 
+            v-if="loop_page >= answer_page - 5 && loop_page <= answer_page + 5" 
+            :class="{ active: loop_page === answer_page }">
+          <button class="page-link"
+                  @click="$store.commit('setAnswerPage', loop_page)">{{ loop_page + 1 }}</button>
         </li>
       </template>
       <li class="page-item" :class="{ disabled: answer_page >= totalPage - 1 }">
-        <button class="page-link" @click="this.$store.dispatch('setAnswerPage', answer_page + 1)">다음</button>
+        <button class="page-link" 
+                @click="$store.commit('setAnswerPage', answer_page + 1)">다음</button>
       </li>
     </ul>
     <!-- 페이징처리 끝 -->
-
     <!-- 답변 등록 -->
     <form @submit.prevent="postAnswer" class="my-3">
+    <ErrorComponent :error="error" />
       <div class="mb-3">
-        <textarea rows="10" v-model="content" 
-               class="form-control"
-               :class="{ 'disabled': !is_login }"></textarea>
+         <textarea rows="10" v-model="content" 
+                   class="form-control"
+                   :disabled="!is_login"
+                   ></textarea>
       </div>
-      <input type="submit" value="답변등록" class="btn btn-primary" :class="{ 'disabled': !is_login }">
+         <input type="submit" value="답변등록" class="btn btn-primary" 
+                :class="{ 'disabled': !is_login }">
     </form>
 </div>
   
@@ -148,13 +122,21 @@
 
 <script>
 import fastapi from '../lib/api';
+import ErrorComponent from "../components/ErrorComponent.vue"
+import CommentQuestion from "../components/CommentQuestion.vue"
+import CommentAnswer from "../components/CommentAnswer.vue"
+import { marked } from 'marked';
 import moment from 'moment';
 import 'moment/locale/ko';
-import { parse } from 'marked';
 
 moment.locale('ko')
 
 export default {
+  components: {
+    ErrorComponent,
+    CommentQuestion,
+    CommentAnswer
+  },  
   props: {
     question_id: {
       type: String,
@@ -163,38 +145,35 @@ export default {
   },
   data() {
     return {
-      question: { answers: [], voter:[], content:'', comments:[]  },
+      question: { answers: [], voter:[], content: ""},
       content: "",
+      error: {detail:[]},
       answerList: [],
       size: 5,
       total: 0,
-      showCommentInput: false, // 댓글 입력창 토글 상태
-      commentContent: "", // 댓글 내용
-      isEditing: false, // 댓글 수정창 토글 상태
-      editIndex: -1, // 수정할 댓글의 인덱스
     };
   },
   computed: {
       is_login() {
         return this.$store.state.is_login;
       },
-      markContent() {
-        return (content) => parse(content);
-      },
       totalPage() {
         return Math.ceil(this.total / this.size);
       },
-      answer_page() { 
-          return this.$store.state.answer_page 
+      answer_page() {
+          return this.$store.state.answer_page
       },
-      sort_by() { 
-          return this.$store.state.sort_by 
+      sort_by() {
+          return this.$store.state.sort_by
       },
       desc() {
-           return this.$store.state.desc 
+           return this.$store.state.desc
       },
   },
   methods: {
+    markContent(_content){
+        return marked(_content)
+    },  
     getQuestion() {
       let url = `/api/question/detail/${this.question_id}`
       fastapi('get', url, {}, (json) => {
@@ -207,9 +186,14 @@ export default {
         content: this.content
       }
       fastapi('post', url, params, () => {
-        this.content = ''
-        this.getQuestion()
-       },
+         this.content = ""
+          this.error = { detail: [] }
+          this.getQuestion()
+          this.getAnswerList()
+        },
+        (err_json) => {
+          this.error = err_json
+        }
       )
     },
     formatDate(date) {
@@ -223,7 +207,10 @@ export default {
         }
         fastapi('delete', url, params, () => {
           this.$router.push("/")
-        })    
+        },
+        (err_json) => {
+          this.error = err_json
+        })
       }
     },
     deleteAnswer(answer_id) {
@@ -234,6 +221,10 @@ export default {
         }
         fastapi('delete', url, params, () => {
             this.getQuestion()
+            this.getAnswerList()
+        },
+        (err_json) => {
+          this.error = err_json
         })
       }
     },
@@ -245,6 +236,9 @@ export default {
         }
         fastapi('post', url, params, () => {
             this.getQuestion()
+        },
+        (err_json) => {
+          this.error = err_json
         })
       }
     },
@@ -256,12 +250,16 @@ export default {
         }
         fastapi('post', url, params, () => {
             this.getQuestion()
+            this.getAnswerList()
+        },
+        (err_json) => {
+          this.error = err_json
         })
       }
     },
     getAnswerList() {
       let url = "/api/answer/list"
-      let params = { 
+      let params = {
           question_id: this.question_id,
           page: this.answer_page,
           size: this.size,
@@ -270,65 +268,13 @@ export default {
       }
       fastapi('get', url, params, (json) => {
         this.answerList = json.answer_list;
-        this.total = json.total;
-      });
+        this.total = json.total
+      },
+      (err_json) => {
+        this.error = err_json
+      })
     },
-    toggleCommentInput() {
-      this.showCommentInput = !this.showCommentInput;
-      this.commentContent = ""; // 댓글 내용 초기화
-    },
-    startEditing(index) {
-      this.editIndex = index;
-      this.isEditing = true;
-    },
-    stopEditing() {
-      this.isEditing = false;
-      this.editIndex = -1;
-    },
-
-    postCommentQuestion() {
-      let url = `/api/comment/create/question/${this.question_id}`
-      let params = {
-        content: this.commentContent
-      }
-      fastapi('post', url, params, () => {
-        this.commentContent = ''
-        this.getQuestion()
-        this.getAnswerList()
-       },
-      ),
-      this.toggleCommentInput()
-    },
-    
-    updateComment() {
-      const comment = this.question.comments[this.editIndex]
-      let url = `/api/comment/update`
-      let params = {
-         content: comment.content
-      }
-      fastapi('put', url, params, () => {
-        this.commentContent = ''
-       },
-      ),
-      this.getQuestion()
-      this.getAnswerList()
-      this.stopEditing()
-    },
-    deleteComment(commentId) {
-      let url = `/api/comment/delete`
-      let params = {
-           comment_id: commentId
-      }
-      fastapi('delete', url, params, () => {
-        this.getQuestion()
-        this.getAnswerList()
-       },
-      ),
-      this.stopEditing()
-    },
-    
-
-  },  
+  },
   watch: {
     answer_page() {
         this.getAnswerList();
@@ -339,12 +285,12 @@ export default {
     desc() {
         this.getAnswerList();
     }
-  },
+  },  
   created() {
     this.getQuestion();
-    this.$store.dispatch('setAnswerPage', 0)
-    this.$store.dispatch('setSortBy', 'create_date');
-    this.$store.dispatch('setDesc', true)
+    this.$store.commit('setAnswerPage', 0)
+    this.$store.commit('setSortBy', 'create_date');
+    this.$store.commit('setDesc', true)
     this.getAnswerList();
   }
 }
