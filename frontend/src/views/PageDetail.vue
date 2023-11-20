a13ae740a48b7ed2a21c3122feb111c9411ba917
kjrstory <kjrstory@gmail.com>
2023-11-20 13:26:57 +0900

    date format fix

--- frontend/src/views/PageDetail.vue
+++ frontend/src/views/PageDetail.vue
@@ -89,7 +89,7 @@ export default {
       )
     },
     formatDate(date) {
-      return moment(date).format('YYYY년 MM월 DD일 HH:mm:ss');
+      return moment(date).format('YYYY년 MM월 DD일 hh:mm a');
     }
   },  
   created() {
