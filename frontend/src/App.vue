01f9a81de343d309c8d8f549cf495fc1960d3fc1
kjrstory <kjrstory@gmail.com>
2023-11-21 13:04:13 +0900

    Sidebar test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <NavigationBar />
-  <router-view/>  
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-2">
+        <SideBar />
+      </div>
+      <main role="main" class="col-md-10">
+        <router-view />
+      </main>
+    </div>
+  </div>
 </template>
 
 <script>
 import NavigationBar from "./components/NavigationBar.vue"
+import SideBar from "./components/SideBar.vue"
 
 export default {
   name: 'App',
   components: {
-     NavigationBar
+     NavigationBar,
+     SideBar
   }
 }
 </script>
---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="d-flex flex-column flex-shrink-0 p-3 text-black bg-light" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-black text-decoration-none">
+      <span class="fs-4">카테고리</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link active fs-5" aria-current="page" >
+          모두
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-black fs-5">
+          질문답변
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-black fs-5">
+          강좌
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-black fs-5">
+          자유게시판
+        </a>
+      </li>
+    </ul>
+    <hr>
+  </div>
+</template>
